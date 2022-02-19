01679bf41228038e50346e8fb363695db43ad17b
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-19 18:58:21 +0200

    testing database connection and CRUD

--- Go Lang/main.go
+++ Go Lang/main.go
@@ -21,14 +21,12 @@ func main() {
 
 	defer db.Close()
 
-	// perform a db.Query insert
-	insert, err := db.Query("INSERT INTO blocked VALUES (5,2,3)")
+	insert, err := db.Query("INSERT INTO blocked VALUES (NULL,2,1)")
 
-	// if there is an error inserting, handle it
 	if err != nil {
 		panic(err.Error())
 	}
-	// be careful deferring Queries if you are using transactions
 	defer insert.Close()
+	fmt.Println("Success")
 
 }
