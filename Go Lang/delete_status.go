69002d04d318afd117a60fb0b76262478da2048d
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-20 00:22:43 +0200

    Delete Status API returns a JSON

--- Go Lang/delete_status.go
+++ Go Lang/delete_status.go
@@ -13,7 +13,6 @@ import (
 )
 
 var (
-	// flagPort is the open port the application listens on
 	flagPort = flag.String("port", "9000", "Port to listen on")
 )
 
@@ -32,36 +31,29 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		r.ParseForm()
 		post_id := r.Form.Get("post_id")
-
-		//fmt.Println(testapi.Test())
 		fmt.Println("Go MySQL Tutorial")
 		db, err := sql.Open("mysql", "root:@/facebookdb")
-
 		if err != nil {
 			panic(err.Error())
 		}
-
 		defer db.Close()
-
 		insert, err := db.Query("DELETE FROM posts WHERE id=?;", post_id)
-
 		if err != nil {
 			panic(err.Error())
+		} else {
+			resp := make(map[string]string)
+			resp["status"] = "Post Deleted!"
+			jsonResp, err := json.Marshal(resp)
+			if err != nil {
+				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+			}
+			defer insert.Close()
+			w.Write(jsonResp)
 		}
-		defer insert.Close()
-		resp := make(map[string]string)
-		resp["status"] = "Post Deleted!"
-		jsonResp, err := json.Marshal(resp)
-		if err != nil {
-			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
-		}
-		w.Write(jsonResp)
-		fmt.Println("Success")
 
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
-
 }
 
 func init() {
